66e3178c1517dee5be01926b48dbcbd1908a3712
samiahmedsiddiqui <sami.siddiqui@yasglobal.com>
2019-11-14 12:03:31 +0500

    Changing runkit filename and remove process.exit command

--- test/runkitExample.js
+++ test/runkitExample.js
@@ -5,5 +5,3 @@ console.log('Without Trim: ', str);
 console.log('With lTrim: ', trim.lTrim(str));
 console.log('With rTrim: ', trim.rTrim(str));
 console.log('With trimPhp: ', trim.trimPhp(str));
-
-process.exit();
